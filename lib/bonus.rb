f4def3c568cddb4424f8114912fb1a4df4a6dce5
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-20 08:25:07 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,11 +20,10 @@ def bonus
    }
   }
 
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-
-
+    epic_tragedy[:montague][:hero][:status] = "dead"
+    epic_tragedy[:capulet][:heroine][:status] = "dead"
+    
+    epic_tragedy
   
-   epic_tragedy
 end
