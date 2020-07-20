bb1974d5bc90827b92dba9834c5595e341cfaa88
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-20 08:24:52 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
 
-    epic_tragedy[:Romeo][:hero][:status] = "dead"
-    epic_tragedy[:Juliette][:heroine][:status] = "dead"
+    epic_tragedy[:montague][:hero][:status] = "dead"
+    epic_tragedy[:capulet][:heroine][:status] = "dead"
     
     epic_tragedy
   
